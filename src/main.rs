e93f920002de3dcfd9aaec72acf51d98306e5a8a
NoPantsSay <972844572@qq.com>
2026-04-07 16:18:19 +0800

    clean && change the project name to chiikawa

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .run();
 }

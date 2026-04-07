8bc97037aed727a5db10e18fd44f9ae5b63c4de2
NoPantsSay <972844572@qq.com>
2026-04-07 17:22:31 +0800

    add freecamera and fps plugin

---
+++ src/cameras/free_camera.rs
@@ -0,0 +1,25 @@
+use bevy::camera_controller::free_camera;
+use bevy::prelude::*;
+
+pub struct FreeCameraPlugin;
+impl Plugin for FreeCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(free_camera::FreeCameraPlugin);
+        app.add_systems(Startup, spawn_camera);
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(5.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        // Free camera controller settings
+        free_camera::FreeCamera {
+            sensitivity: 0.2,
+            friction: 25.0,
+            walk_speed: 3.0,
+            run_speed: 9.0,
+            ..default()
+        },
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig};
 use bevy::prelude::*;
+use chiikawa::cameras::free_camera::FreeCameraPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FpsOverlayPlugin {
+            config: FpsOverlayConfig {
+                frame_time_graph_config: FrameTimeGraphConfig {
+                    enabled: false,
+                    ..default()
+                },
+                ..default()
+            },
+        })
+        .add_plugins(FreeCameraPlugin)
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
     commands.spawn(Sprite {
         image: asset_server.load("ducky.png"),
         ..Default::default()

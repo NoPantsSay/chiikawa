826cdb5dbe091292e353adf8cc2f7c4a62c780f0
NoPantsSay <972844572@qq.com>
2026-04-20 21:33:22 +0800

    add fast_poisson

--- src/cameras/free_camera.rs
+++ src/cameras/free_camera.rs
@@ -1,20 +1,35 @@
+// 在你的 camera 插件模块中
 use bevy::camera::Exposure;
 use bevy::camera_controller::free_camera;
 use bevy::prelude::*;
 
-pub struct FreeCameraPlugin;
+pub struct FreeCameraPlugin {
+    pub initial_transform: Transform,
+}
+
+impl Default for FreeCameraPlugin {
+    fn default() -> Self {
+        Self {
+            initial_transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        }
+    }
+}
+
 impl Plugin for FreeCameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(free_camera::FreeCameraPlugin);
+        app.insert_resource(CameraInitialTransform(self.initial_transform));
         app.add_systems(Startup, spawn_camera);
     }
 }
 
-fn spawn_camera(mut commands: Commands) {
+#[derive(Resource)]
+struct CameraInitialTransform(Transform);
+
+fn spawn_camera(mut commands: Commands, initial_transform: Res<CameraInitialTransform>) {
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        // Free camera controller settings
+        initial_transform.0,
         free_camera::FreeCamera {
             sensitivity: 0.2,
             friction: 25.0,
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 use bevy_mod_outline::*;
 use bevy_wind_waker_shader::prelude::*;
 use chiikawa::cameras::free_camera::FreeCameraPlugin;
-use rand::RngExt;
+use fast_poisson::Poisson2D;
 
 #[derive(Component)]
 struct Grass;
@@ -28,39 +28,17 @@ fn main() {
         })
         .add_plugins(WindWakerShaderPlugin::default())
         .add_plugins(OutlinePlugin)
-        .add_plugins(FreeCameraPlugin)
-        .add_systems(Startup, setup)
+        .add_plugins(FreeCameraPlugin::default())
+        .add_systems(Startup, (setup, setup_grass))
         .add_systems(Update, rotation_grass)
         .run();
 }
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let grass_texture_handle = asset_server.load("grass0.png");
-    let grass_mesh = meshes.add(Plane3d::default().mesh().size(1.0, 1.0));
-    let grass_material = materials.add(StandardMaterial {
-        base_color_texture: Some(grass_texture_handle),
-        base_color: Color::WHITE,
-        alpha_mode: AlphaMode::Blend,
-        ..default()
-    });
-
-    let mut rng = rand::rng();
-    for _ in 0..10 {
-        let x = rng.random_range(-4.0..=4.0);
-        let z = rng.random_range(-4.0..=4.0);
-        commands.spawn((
-            Mesh3d(grass_mesh.clone()),
-            MeshMaterial3d(grass_material.clone()),
-            Transform::from_xyz(x, 0.001, z),
-            Grass,
-        ));
-    }
-
     commands.spawn((
         Mesh3d(meshes.add(Sphere::new(1.0).mesh().uv(32, 18))),
         Transform::from_xyz(0.0, 1.0, 0.0),
@@ -94,6 +72,36 @@ fn setup(
         },
         Transform::from_translation(Vec3::new(0.0, 1.0, 0.0)).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+}
+
+fn setup_grass(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let grass_texture_handle = asset_server.load("grass0.png");
+    let grass_mesh = meshes.add(Plane3d::default().mesh().size(1.0, 1.0));
+    let grass_material = materials.add(StandardMaterial {
+        base_color_texture: Some(grass_texture_handle),
+        base_color: Color::WHITE,
+        alpha_mode: AlphaMode::Blend,
+        ..default()
+    });
+
+    let points: Vec<[f64; 2]> = Poisson2D::new().with_dimensions([8.0, 8.0], 2.0).generate();
+
+    for &[x, z] in points.iter().take(10) {
+        let x = x as f32 - 4.0;
+        let z = z as f32 - 4.0;
+
+        commands.spawn((
+            Mesh3d(grass_mesh.clone()),
+            MeshMaterial3d(grass_material.clone()),
+            Transform::from_xyz(x, 0.001, z),
+            Grass,
+        ));
+    }
 }
 
 fn rotation_grass(
